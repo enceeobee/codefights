dce1afe52a13b0e3c0bdd7cd899b631e661bfe21
Nate Bisbee <natebot@natebot.local>
2017-06-11 15:11:09 -0400

    Solved "comfortable numbers".

--- arcade/the-core/6-labyrinth of-nested-loops/comfortable.js
+++ arcade/the-core/6-labyrinth of-nested-loops/comfortable.js
@@ -2,18 +2,31 @@
 
 function comfortableNumbers(l, r) {
   'use strict';
+
   const digitSum = digit => (
     String(digit)
       .split('')
       .reduce((sum, val) => sum + Number(val), 0)
   );
-  console.log(l);
-  const rangeStart = l - digitSum(l);
-  const rangeEnd = r + digitSum(r);
 
-  console.log(rangeStart, rangeEnd);
+  const comfyNumbers = {};
+  let pairs = 0;
+  let sum;
 
-  const comfyNumbers = [];
+  for (let i = l; i <= r; i += 1) {
+    comfyNumbers[i] = [];
+    for (let j = i - digitSum(i); j <= i + digitSum(i); j += 1) {
+      if (j !== i) comfyNumbers[i].push(j);
+    }
+  }
+
+  for (let i = l; i < r; i += 1) {
+    for (let j = i + 1; j <= r; j += 1) {
+      if (comfyNumbers[i].includes(j) && comfyNumbers[j].includes(i)) pairs += 1;
+    }
+  }
+
+  return pairs;
 }
 
 let expected;
@@ -30,6 +43,30 @@ assert.equal(actual, expected);
 l = 1;
 r = 9;
 expected = 20;
+actual = comfortableNumbers(l, r);
+assert.equal(actual, expected);
+
+l = 13;
+r = 13;
+expected = 0;
+actual = comfortableNumbers(l, r);
+assert.equal(actual, expected);
+
+l = 12;
+r = 108;
+expected = 707;
+actual = comfortableNumbers(l, r);
+assert.equal(actual, expected);
+
+l = 239;
+r = 777;
+expected = 6166;
+actual = comfortableNumbers(l, r);
+assert.equal(actual, expected);
+
+l = 1;
+r = 1000;
+expected = 11435;
 actual = comfortableNumbers(l, r);
 assert.equal(actual, expected);
 
