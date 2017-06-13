705005273af1a9b313b75df38a5d3df89bf75478
Nate Bisbee <nbisbee@invaluable.com>
2017-06-13 17:03:43 -0400

    Solved Swap Adjacent Bits (the hacky way).

---
+++ arcade/the-core/3-corner-of-0s-and-1s/swapAdjacentBits.js
@@ -0,0 +1,46 @@
+const { equal: doEqual } = require('assert');
+
+function swapAdjacentBits(n) {
+  return parseInt(
+    n.toString(2)
+    .replace(n.toString(2)[0], (n.toString(2).length % 2 === 1) ? `0${n.toString(2)[0]}` : n.toString(2)[0])
+    .split('')
+    .reduce((acc, val, i, arr) => acc + ((i % 2 === 0) ? arr[i + 1] : arr[i - 1]), '')
+  , 2);
+}
+
+let n;
+let expected;
+let actual;
+
+n = 13;
+expected = 14;
+actual = swapAdjacentBits(n);
+doEqual(actual, expected);
+
+n = 74;
+expected = 133;
+actual = swapAdjacentBits(n);
+doEqual(actual, expected);
+
+n = 1073741823;
+expected = 1073741823;
+actual = swapAdjacentBits(n);
+doEqual(actual, expected);
+
+n = 0;
+expected = 0;
+actual = swapAdjacentBits(n);
+doEqual(actual, expected);
+
+n = 1;
+expected = 2;
+actual = swapAdjacentBits(n);
+doEqual(actual, expected);
+
+n = 83748;
+expected = 166680;
+actual = swapAdjacentBits(n);
+doEqual(actual, expected);
+
+console.log('All tests passed.');
