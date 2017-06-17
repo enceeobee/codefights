3569f7ad112f315f9d6acbb6e9f4ed9c2df61783
Nate Bisbee <nbisbee@invaluable.com>
2017-06-17 10:58:32 -0400

    WIP - Weak Numbers.

--- arcade/the-core/6-labyrinth of-nested-loops/weak.js
+++ arcade/the-core/6-labyrinth of-nested-loops/weak.js
@@ -1,5 +1,49 @@
 const assert = require('assert');
 
+function getDivisors(num) {
+    /**
+     * start at 64 / 2
+     * then take that dividend and divide it down by all of its divisors
+     * 32 / 2 = 16; 16/2 = 8; 8/2 = 4; 4/2=2; 2/2 = 1; <- all divisors
+     * 32 / 3 BUST
+     * 32 / 4 ALREADY HAVE
+     * 
+     * 12:
+     * 2 - 12/2=6; 6/2=3; 3/2 BUST
+     * 3 - 12/3=4; 4/3 BUST
+     * 4 - 12/4=3; 3/4 BUST
+     * 5 - 12/5 BUST
+     * 6 - ALREADY HAVE
+     * 
+     * 6:
+     * 2 - 6/2=3; 3/2=BUST
+     * 3 - ALREADY HAVE
+     */
+
+    let divisors = {};
+    let divisor;
+
+    // 1 and num are always divisors (unless num is 1)
+    divisors[1] = true;
+    if (num !== 1) divisors[num] = true;
+
+    for (let i = 2; i <= Math.ceil(num / 2); i += 1) {
+      if (num % i !== 0 || divisors.hasOwnProperty(i)) continue;
+
+      divisors[i] = true;
+      divisor = num / i;
+      while (divisor >= i) {
+        divisors[divisor] = true;
+        if (divisor % i === 0) divisor /= i;
+        else break;
+      }
+    }
+
+    // console.log(divisors);
+    
+    return Object.keys(divisors).length;
+  }
+
 function weakNumbers(n) {
   'use strict';
   const weaknessMap = {};
@@ -9,14 +53,17 @@ function weakNumbers(n) {
   let maxWeakness = 0;
 
   // Smelly brute force
-  function getDivisors(num) {
-    let numDivisors = 0;
-    for (let i = 1; i <= Math.floor(num / 2); i += 1) {
-      if (num % i === 0) numDivisors += 1;
+  /**
+   *       
+    function getDivisors(num) {
+      let numDivisors = 0;
+      for (let i = 1; i <= Math.floor(num / 2); i += 1) {
+        if (num % i === 0) numDivisors += 1;
+      }
+      return numDivisors + 1;
     }
-    return numDivisors + 1;
-  }
-
+  */
+  
   /**
    * To get weakness:
    * 1. calculate divisors for n
@@ -64,7 +111,7 @@ function weakNumbers(n) {
     weaknessMap[weakness].push(i);
   }
 
-  console.log('weaknessMap', weaknessMap);
+  // console.log('weaknessMap', weaknessMap);
   // console.log('divisorMap', divisorMap);
 
   return [maxWeakness, weaknessMap[maxWeakness].length];
@@ -79,29 +126,44 @@ expected = [2, 2];
 actual = weakNumbers(n);
 assert.deepEqual(actual, expected);
 
-// n = 1;
-// expected = [0, 1];
-// actual = weakNumbers(n);
-// assert.deepEqual(actual, expected);
+n = 1;
+expected = [0, 1];
+actual = weakNumbers(n);
+assert.deepEqual(actual, expected);
 
-// n = 2;
-// expected = [0, 2];
-// actual = weakNumbers(n);
-// assert.deepEqual(actual, expected);
+n = 2;
+expected = [0, 2];
+actual = weakNumbers(n);
+assert.deepEqual(actual, expected);
 
-// n = 7;
-// expected = [2, 1];
-// actual = weakNumbers(n);
-// assert.deepEqual(actual, expected);
+n = 7;
+expected = [2, 1];
+actual = weakNumbers(n);
+assert.deepEqual(actual, expected);
 
 n = 500;
 expected = [403, 1];
 actual = weakNumbers(n);
 assert.deepEqual(actual, expected);
 
-// n = 4;
-// expected = [0, 4];
-// actual = weakNumbers(n);
-// assert.deepEqual(actual, expected);
+n = 4;
+expected = [0, 4];
+actual = weakNumbers(n);
+assert.deepEqual(actual, expected);
+
+assert.equal(getDivisors(1), 1);
+assert.equal(getDivisors(2), 2);
+assert.equal(getDivisors(3), 2);
+assert.equal(getDivisors(4), 3);
+assert.equal(getDivisors(5), 2);
+assert.equal(getDivisors(6), 4);
+assert.equal(getDivisors(7), 2);
+assert.equal(getDivisors(8), 4);
+assert.equal(getDivisors(9), 3);
+assert.equal(getDivisors(420), 24);
+assert.equal(getDivisors(360), 24);
+assert.equal(getDivisors(480), 24);
+assert.equal(getDivisors(240), 20);
+assert.equal(getDivisors(288), 18);
 
 console.log('All tests passed.');
