e66947d34184f7248496d6cc2fbe7bb5386f9844
Nate Bisbee <nathanbisbee@invisionapp.com>
2017-08-01 15:48:50 -0400

    Still WIP - But basic idea is there.

--- arcade/the-core/6-labyrinth of-nested-loops/crosswordFormation.js
+++ arcade/the-core/6-labyrinth of-nested-loops/crosswordFormation.js
@@ -22,16 +22,15 @@ function crosswordFormation(words) {
   let intersection2;
   let intersection3;
 
-  const squareCoords1 = [0, 0];
-  let squareCoords2;
-  let squareCoords3;
+  // const squareCoords1 = [0, 0];
+  // let squareCoords2;
+  // let squareCoords3;
 
   // Step 1
-  for (let i = 0; i < words[0].length; i += 1) {
+  for (let i = 0; i < words[0].length - 2; i += 1) {
     intersection1 = findIntersection(words[0][i], words[1])
     if (intersection1 !== -1) {
       intersection1 = [i, intersection1]
-      // console.log('first intersection', intersection1);
 
       // Step 2
       for (let j = intersection1[1] + 2; j < words[1].length; j += 1) {
@@ -39,44 +38,32 @@ function crosswordFormation(words) {
         if (intersection2 !== -1) {
           intersection2 = [j, intersection2];
           const yDiff = j - intersection1[1];
-          squareCoords2 = [0, yDiff]
-
-          // console.log('testIntersection2', testIntersection2);
-          // console.log('second intersection', intersection2);
+          // squareCoords2 = [0, yDiff]
 
           // Step 3
           for (let k = intersection2[1] + 2; k < words[2].length; k += 1) {
             intersection3 = findIntersection(words[2][k], words[3]);
-            const xDiff = k - intersection1[0];
+            const xDiff = k - intersection2[1];
             if (intersection3 !== -1) {
               intersection3 = [k, intersection3]
-              // squareCoords3 = [intersection3[1] - k, squareCoords2[1]];
-              squareCoords3 = [xDiff, squareCoords2[1]];
+              // squareCoords3 = [xDiff, squareCoords2[1]];
               console.log(intersection1, intersection2, intersection3);
-              console.log(squareCoords1, squareCoords2, squareCoords3);
-              // console.log('diff in x from first word to fourth word is', intersection3[0] - intersection2[1])
+              // console.log(squareCoords1, squareCoords2, squareCoords3);
 
               // Check fourth intersection. If the letter of the first word matches the letter
-              // of the fourth word, BOOM!
+              // of the fourth word, increment return value!
               const firstWordLetter = words[0][i + xDiff];
-              const fourthWordLetter = words[3][words[3].length - yDiff - 1];
+              const fourthWordLetter = words[3][intersection3[1] - yDiff];
 
-              // console.log('diff in Y from first word to third word is', j - intersection1[1])
-              console.log('diff in Y from first word to third word is', yDiff);
-              console.log('diff in X from second word to fourth word is', xDiff);
-              // console.log('Thus, the fourth intersection letter is', words[0][i + intersection3[1] - k])
+              console.log('xDiff', xDiff);
+              console.log('yDiff', yDiff);
               console.log('firstWordLetter', firstWordLetter, 'fourthwordletter', fourthWordLetter);
-              console.log('\n');
-              // console.log('intersection1', intersection1);
-              // console.log('intersection2', intersection1);
-              // console.log('intersection3', intersection3);
-              // [1,2], [7,1], [3,5] ([3,0])
-              // TODO - Check fourth intersection also
-              let doesFourthIntersectionWork = false;
-              if ('something') {
-                doesFourthIntersectionWork = true;
+
+              if (firstWordLetter && firstWordLetter === fourthWordLetter) {
+                numXwords += 1;
+                // console.log('numXwords', numXwords)
               }
-              if (doesFourthIntersectionWork) numXwords += 1;
+              console.log('\n');
             }
           }
         }
@@ -88,7 +75,16 @@ function crosswordFormation(words) {
 
 const test = (words, expected) => assert.equal(crosswordFormation(words), expected);
 
-test(['crossword', 'formation', 'something', 'square'], 6);
+// TEST tests:
+test(['crossword', 'formation', 'something', 'square'], 1);
+test(['crossword', 'formation', 'square', 'something'], 1);
+test(['crossword', 'square', 'something', 'formation'], 1);
+
+test(['formation', 'crossword', 'square', 'something'], 1);
+test(['formation', 'crossword', 'something', 'square'], 1);
+test(['square', 'crossword', 'formation', 'something'], 1);
+// End test tests
+
 // test(['crossword', 'square', 'formation', 'something'], 6);
 // test(['anaesthetist',
 //  'thatch',
