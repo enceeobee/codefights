6e95c4cea26d2f86c60672f1b125acfc2a3351cc
Nate Bisbee <nathanbisbee@invisionapp.com>
2017-09-06 09:09:54 -0500

    WIP - update createAnagram

--- arcade/the-core/8-mirror-lake/createAnagram.js
+++ arcade/the-core/8-mirror-lake/createAnagram.js
@@ -22,7 +22,7 @@ function createAnagram(s, t) {
    * tMap = { A: 2, C: 1 }
    *
    * sNeeds = ['C']
-   * sDontNeed = ['B']
+   * sExtras = ['B']
    *
    * sMap needs C, doesn't need B
    * TURN - swap C with B
@@ -32,13 +32,15 @@ function createAnagram(s, t) {
    *
    * return 1
    */
+  let turns = 0
   const isAnagram = (a, b) => (
-    Object.keys(a).every(key => b[key] = a[key])
+    Object.keys(a).every(key => b[key] === a[key])
   )
+
   const sNeeds = []
-  const sDontNeed = []
+  const sExtras = []
   const sMap = s.split('').reduce((acc, val) => {
-    if (!t.includes(val)) sDontNeed.push(val)
+    if (!t.includes(val)) sExtras.push(val)
     if (!acc[val]) acc[val] = 0
     acc[val] += 1
     return acc
@@ -50,17 +52,86 @@ function createAnagram(s, t) {
     return acc
   }, {})
 
-  console.log(sMap)
-  console.log(tMap)
-  console.log(sNeeds, sDontNeed)
+  // console.log('sMap', sMap)
+  // console.log('tmap', tMap)
+  // console.log('needs', sNeeds, 'extras', sExtras)
+
+  let turnsTaken
+  let swapMap
+  let swapLetter
+  while (!isAnagram(sMap, tMap)) {
+    swapMap = {}
+
+    // Take turns
+    turnsTaken = 1
+
+    // First, convert sExtras to sNeeds
+    if (sNeeds.length > 0) {
+      sMap[sNeeds[0]] = tMap[sNeeds[0]]
+      turnsTaken = tMap[sNeeds[0]]
+
+      // TODO - Should we only delete this if sMap[sExtras[0]] is 1? I can't think right now
+      delete sMap[sExtras[0]]
+
+      sNeeds.shift()
+      sExtras.shift()
+
+      // console.log('needs', sNeeds, 'extras', sExtras)
+      // console.log('smap', sMap)
+
+    }
+
+    // Second, adjust the number of common letters (ex. sMap { A: 4, B: 1 }, tmap { A: 3, B: 2 })
+    Object.keys(tMap).forEach((key) => {
+      // console.log('key', key)
+      if (tMap[key] !== sMap[key]) {
+        // Swap letters
+        // swapMap[key] = Math.abs(tMap[key] - sMap[key])
+        if (!swapLetter) {
+          swapLetter = key
+          return
+        }
+
+        // console.log('swapLetter', swapLetter)
+
+        // if (tMap[key] > sMap[key]) {
+        //   tMap[key] -= 1
+        //   tMap[swapLetter] += 1
+        // } else {
+        //   tMap[key] += 1
+        //   tMap[swapLetter] -= 1
+        // }
+        if (sMap[key] > tMap[key]) {
+          sMap[key] -= 1
+          sMap[swapLetter] += 1
+        } else {
+          sMap[key] += 1
+          sMap[swapLetter] -= 1
+        }
+      }
+
+      console.log('smap', sMap, 'tmap', tMap)
+    })
+
+    turns += turnsTaken
+
+    // console.log('smap', sMap, 'tmap', tMap)
+
+    // TODO - Obviously remove this
+    break
+  }
+
+  return turns
 }
 
 const test = (s, t, ex) => assert.equal(createAnagram(s, t), ex)
 
-test('AABAA', 'BBAAA', 1)
+// test('AABAA', 'BBAAA', 1)
+// test('BBAAA', 'AABAA', 1)
 test('OVGHK', 'RPGUC', 4)
-test('AAB', 'AAC', 1)
-test('NATE', 'ANTE', 0)
-test('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB', 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAC', 1)
+// test('OVGHHK', 'RPGUCF', 5)// this could be wrong, the point is it'll take two turns to get both H's
+// test('AAB', 'AAC', 1)
+// test('NATE', 'ANTE', 0)
+// test('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB', 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAC', 1)
 
 console.log('All tests passed.')
