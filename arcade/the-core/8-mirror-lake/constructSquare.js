50bd0d3e752945415cae85e8ad036372855eab1b
Nate Bisbee <ncob@users.noreply.github.com>
2017-09-10 17:40:04 -0500

    Solve Construct Square

---
+++ arcade/the-core/8-mirror-lake/constructSquare.js
@@ -0,0 +1,40 @@
+const assert = require('assert')
+
+function constructSquare (s) {
+  const makeMap = value => (
+    String(value).split('')
+      .reduce((acc, val) => {
+        if (!acc.includes(val)) acc.push(val)
+        return acc
+      }, [])
+      .map(key => String(value).match(new RegExp(key, 'g')).length)
+      .sort((a, b) => a - b)
+    )
+  const checkDoesMatch = (sMap, nMap) => sMap.length === nMap.length && sMap.every((val, i) => val === nMap[i])
+  const sMap = makeMap(s)
+
+  let highSquare = 0
+  let curSqrt = Math.floor(Math.sqrt((Math.pow(10, s.length - 1))))
+  let curSquare = curSqrt * curSqrt
+
+  while (String(curSquare).length <= s.length) {
+    if (checkDoesMatch(sMap, makeMap(curSquare)) && curSquare > highSquare) {
+      highSquare = curSquare
+    }
+    curSqrt += 1
+    curSquare = curSqrt * curSqrt
+  }
+
+  return highSquare || -1
+}
+
+const test = (s, expected) => assert.equal(constructSquare(s), expected)
+
+test('ab', 81)
+test('zzz', -1)
+test('aba', 900)
+test('abcbbb', 810000)
+test('abc', 961)
+test('aaaabbcde', 999950884)
+
+console.log('All tests passed.')
