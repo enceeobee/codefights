b664b20e571c0a8d2956c81cbd66c3f089b35c9d
Nate Bisbee <nathanbisbee@invisionapp.com>
2017-09-26 15:21:51 -0500

    Solve minimal number of coins

---
+++ arcade/the-core/9-well-of-integration/minimalNumberOfCoins.js
@@ -0,0 +1,31 @@
+const assert = require('assert')
+
+function minimalNumberOfCoins (coins, price) {
+  let n = 0
+  let priceLeft = price
+  let i = coins.length - 1
+
+  while (priceLeft > 0) {
+    while (priceLeft < coins[i]) --i
+    priceLeft -= coins[i]
+    n += 1
+  }
+
+  return n
+}
+
+const test = (c, p, x) => assert.equal(minimalNumberOfCoins(c, p), x)
+
+test([1, 2, 10], 28, 6)
+test([1, 5, 10, 100], 239, 10)
+test([1], 28, 28)
+test([1], 2, 2)
+test([2], 2, 1)
+test([1, 3], 28, 10)
+test([1, 2], 28, 14)
+test([1, 2, 12], 28, 4)
+test([2, 4], 17, 5)
+test([2], 3, 2)
+test([1, 10, 100], 14, 5)
+
+console.log('All tests passed')
