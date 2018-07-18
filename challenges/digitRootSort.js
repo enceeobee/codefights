46d3a3b6514d3e8c646fef987473f293adf1c29d
Nate Bisbee <ncob@users.noreply.github.com>
2018-07-17 21:35:52 -0400

    Solve digitRootSort

---
+++ challenges/digitRootSort.js
@@ -0,0 +1,30 @@
+const assert = require('assert')
+
+function digitRootSort (a) {
+  const aItemsWithDigitRoot = a.map((item) => {
+    return {
+      value: item,
+      digitRoot: String(item).split('').reduce((acc, val) => acc + Number(val), 0)
+    }
+  })
+
+  aItemsWithDigitRoot.sort((a, b) => {
+    if (a.digitRoot === b.digitRoot) return a.value - b.value
+    return a.digitRoot - b.digitRoot
+  })
+
+  return aItemsWithDigitRoot.map(item => item.value)
+}
+
+const tests = [
+  {
+    a: [13, 20, 7, 4],
+    x: [20, 4, 13, 7]
+  },
+  {
+    a: [100, 22, 4, 11, 31, 103],
+    x: [100, 11, 4, 22, 31, 103]
+  }
+]
+
+tests.forEach(({ a, x }) => assert.deepEqual(digitRootSort(a), x))
