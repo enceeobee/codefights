94af76bfa0db8594ccfd3eb081016778ad99c755
Nate Bisbee <enceeobee@users.noreply.github.com>
2019-07-10 08:19:52 -0700

    Remove comments

--- interview-practice/hash-tables/possibleSums.js
+++ interview-practice/hash-tables/possibleSums.js
@@ -32,47 +32,6 @@ function possibleSums (coins, quantity) {
   return possibleSumCount
 }
 
-/*
-function possibleSums (coins, quantity) {
-  const sumMemo = {}
-  let possibleSumCount = 0
-
-  const memoize = (sum) => {
-    if (!sumMemo[sum]) {
-      sumMemo[sum] = true
-      possibleSumCount++
-    }
-  }
-
-  const addAndMemo = (adder, values, quantities) => {
-    if (values.length === 0) return
-    if (quantities[0] === 0) return
-
-    // console.log('adder', adder)
-    // console.log('values', values)
-    // console.log('quantities', quantities)
-
-    const newSum = adder + values[0]
-
-    memoize(newSum)
-
-    const slicedValues = values.slice(1)
-    const slicedQuantities = quantities.slice(1)
-    const qCopy = [...quantities]
-    qCopy[0]--
-
-    addAndMemo(adder, slicedValues, slicedQuantities)
-    addAndMemo(adder, values, qCopy)
-
-    addAndMemo(newSum, slicedValues, slicedQuantities)
-    addAndMemo(newSum, values, qCopy)
-  }
-
-  addAndMemo(0, coins, quantity)
-
-  return possibleSumCount
-}
- */
 const makeTest = (c, q, x) => ({ c, q, x })
 const tests = [
   makeTest([10], [2], 2),
@@ -87,12 +46,12 @@ const tests = [
   makeTest([1, 2, 3], [2, 3, 10000], 30008),
   makeTest([3, 1, 1], [111, 84, 104], 521),
   makeTest([1, 1, 1, 1, 1], [9, 19, 18, 12, 19], 77),
-  // // my tests
-  // makeTest([10, 50], [5, 1], 10),
-  // makeTest([5], [5], 5),
-  // makeTest([10, 50], [1, 2], 5), // 10, 60, 110, 50, 100; 10 + sum (10, 0); 10 + sum(50, 2); 10 + sum(50, 1); 10 + sum(50, 0); 50 + sum(50, 1); 50 + sum(50, 0)
-  // makeTest([50, 100], [2, 1], 4), // 50, 100, 150, 200
-  // makeTest([1, 5], [1, 1], 3),
+  // my tests
+  makeTest([10, 50], [5, 1], 10),
+  makeTest([5], [5], 5),
+  makeTest([10, 50], [1, 2], 5), // 10, 60, 110, 50, 100; 10 + sum (10, 0); 10 + sum(50, 2); 10 + sum(50, 1); 10 + sum(50, 0); 50 + sum(50, 1); 50 + sum(50, 0)
+  makeTest([50, 100], [2, 1], 4), // 50, 100, 150, 200
+  makeTest([1, 5], [1, 1], 3),
   makeTest([], [], 0)
 ]
 
